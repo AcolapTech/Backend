fb82ae2af795a39b15de6fc4a924065a4839f302
AcolapTech <future.tech.acolap@gmail.com>
2025-02-05 15:04:37 -0500

    prueba commit  file 1.0

--- app/routes/emp.js
+++ app/routes/emp.js
@@ -6,7 +6,12 @@ import admin from "../middlewares/admin.js";
 
 const router = express.Router();
 
-router.post("/savempImg", upload, auth, controllers.savempImg);
+router.post("/savenewsImg", upload, auth, admin, async (req, res) => {
+    console.log(req.files);  // Muestra los archivos subidos en la terminal
+    console.log(req.body);   // Muestra los datos del formulario
+  
+    res.send({ message: "Archivos recibidos correctamente" });  // Prueba si multer funciona bien
+  });
 router.get("/listallemp", controllers.listallemp);
 router.put("/updateemp", auth, admin, controllers.updateemp);
 router.get("/findemp/:nombre", controllers.findemp);
